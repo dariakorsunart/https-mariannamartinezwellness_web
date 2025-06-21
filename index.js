4ff1fb6b4fe3c352b4a3cfd0a648dafdd3a1d5c3
Kirill Bessonov <kbessonov@gmail.com>
2025-06-21 08:18:04 -0400

    final release with fixed mobile version

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
-const back = document.querySelector("#back");
-const next = document.querySelector("#next");
 
-const photos = ["Testimonial 1 - AnyaAndrei1.png","Testimonial 2 - Anya.png","Testimonial 3 - Mila.png","Testimonial 4 - Alex.jpg","Testimonial 5 - Alex.jpg","Testimonial 6 - Alex.jpg","Testimonial 7 - Michal.jpg","Testimonial 7 - Svetlana2.jpg","Testimonial 8 - ValyaBeforeAfterNew.png","Testimonial 9 - Valya.png","Testimonial 10 - DimaBeforeandAfter.jpg", "Testimonial 11 - Dima.png","Testimonial 12 .png","Testimonial 13 - Alex.png"];
-let i = 0;
 
-if(back !== null && next!==null){
-    next.addEventListener("click",()=>{
-        i++;
-        if (i>photos.length-1) {
-            i=0;
-        }
-        document.querySelector("#pictureTestimonials").src = photos[i];
-    })
+initServicesHorizontalScroll()
+initTestimonialsScroll()
 
-    back.addEventListener("click",()=>{
-        i--;
-        if (i<0) {
-            i=photos.length-1;
-        }
-        document.querySelector("#pictureTestimonials").src = photos[i];
-    })
+function initTestimonialsScroll(){
+  const back = document.querySelector("#back");
+  const next = document.querySelector("#next");
+  
+
+  const photos = ["Testimonial 1 - AnyaAndrei1.png","Testimonial 2 - Anya.png",
+    "Testimonial 3 - Mila.png","Testimonial 4 - Alex.jpg",
+    "Testimonial 5 - Alex.jpg","Testimonial 6 - Alex.jpg",
+    "Testimonial 7 - Michal.jpg","Testimonial 7 - Svetlana2.jpg",
+    "Testimonial 8 - ValyaBeforeAfterNew.png","Testimonial 9 - Valya.png",
+    "Testimonial 10 - DimaBeforeandAfter.jpg", "Testimonial 11 - Dima.png","Testimonial 12.png",
+    "Testimonial 13 - Alex.png", "Testimonial 14 - Ollie.jpeg"];
+  let i = 0;
+  if(back !== null && next!==null){
+      next.addEventListener("click",()=>{
+          i++;
+          if (i>photos.length-1) {
+              i=0;
+          }
+          document.querySelector("#pictureTestimonials").src = photos[i];
+      })
+
+      back.addEventListener("click",()=>{
+          i--;
+          if (i<0) {
+              i=photos.length-1;
+          }
+          document.querySelector("#pictureTestimonials").src = photos[i];
+      })
+  }
 }
-
 
 fetch('navbar.html')
     .then(response => response.text())
@@ -39,4 +51,24 @@ function scrollServices(direction) {
             behavior: 'smooth'
           });
         }
-}  
+}
+
+
+function initServicesHorizontalScroll() {
+  
+  const container = document.querySelector(".imgGrid");
+  const next_arrow = document.querySelector(".scroll-arrow.next");
+  const prev_arrow = document.querySelector(".scroll-arrow.prev");
+
+  if(next_arrow !== null && prev_arrow!==null && container !== null){
+    const scrollAmount = document.querySelector(".imgButton").offsetWidth;
+
+    next_arrow.addEventListener('click', () => {
+      container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    });
+
+    prev_arrow.addEventListener('click', () => {
+      container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+    });
+  }
+}
